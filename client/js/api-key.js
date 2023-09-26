6b2ec4901b01fc523019786ebfca4eb778689894
Arafat Rahaman <96245505+Hieser21@users.noreply.github.com>
2023-09-26 16:24:57 +0600

    Add files via upload

---
+++ client/js/api-key.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", () => {  
+   var myToast = Toastify({
+  text: "This page is still in development, so please expect bugs. Click this toast to join the Discord and keep track of bugs and updates.",
+  duration: 3000,
+  destination: "https://discord.gg/dwarka",
+  newWindow: true,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "linear-gradient(to right, #00b09b, #96c93d)",
+  },
+  onClick: function(){} // Callback after click
+}).showToast();
+});  
+
+(function () {  
+  function get_api_key_from_input() {  
+    return "Rfc6iopkthdVWZqeWORUhZwp6MfqdwqGGSTOd2rYD0c " 
+  }  
+  window.get_api_key_from_input = get_api_key_from_input;
+})();  
